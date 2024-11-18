455e0fdb5f23f197d41d9602e497c08b933433e7
Illia Verezei <useraccont1507@gmail.com>
2024-11-18 18:42:42 +0200

    added localization for FirstViewController.swift

---
+++ MyWeather/Localization/String + extension.swift
@@ -0,0 +1,19 @@
+//
+//  String + extension.swift
+//  MyWeather
+//
+//  Created by Illia Verezei on 18.11.2024.
+//
+
+import Foundation
+
+extension String {
+  var localized: String {
+    NSLocalizedString(self, comment: "\(self) could not be found in Localizable.string")
+  }
+  
+  func localizedPlural(_ arg: Int) -> String {
+    let formatString = NSLocalizedString(self, comment: "\(self) could not be found in Localizable.stringdict")
+    return Self.localizedStringWithFormat(formatString, arg)
+  }
+}
