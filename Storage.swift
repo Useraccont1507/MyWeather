a666313ec98c3111ff38df89ae94b488269f109c
Illia Verezei <useraccont1507@gmail.com>
2024-11-18 18:18:40 +0200

    added checking user first entering app

--- Storage.swift
+++ Storage.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Storage {
+  static let shared = Storage()
   
+  private var storage = UserDefaults.standard
+  
+  private enum Keys: String {
+    case isFirstEnter = "storage_key_enter_12589459492348927489"
+  }
+  
+  private init() {}
+  
+  func isFirstEnter() -> Bool {
+    if storage.value(forKey: Keys.isFirstEnter.rawValue) == nil {
+      storage.set(true, forKey: Keys.isFirstEnter.rawValue)
+      return true
+    }
+    return false
+  }
 }
