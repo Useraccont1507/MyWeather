99e36f9525efdcef5a4c825fa8fc279ce78161af
Illia Verezei <useraccont1507@gmail.com>
2025-02-02 12:23:22 +0200

    created citiesListModule

---
+++ MyWeather/CitiesListModule/Model/CityCellViewModel/CityShortForecastViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CityShortForecastViewModel.swift
+//  MyWeather
+//
+//  Created by Illia Verezei on 01.02.2025.
+//
+
+import UIKit
+
+struct ShortCityForecast {
+  let cityName: String?
+  let tempLabelText: String?
+  let weatherDescriptionLabel: String?
+  let background: CAGradientLayer?
+  let isSuccess: Bool
+}
